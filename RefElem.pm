536ad0edb676900ba3157023a93d87f29683f5c7
Gisle Aas <gisle@aas.no>
2000-01-09 11:14:12 +0000

    Something that actually works.

--- RefElem.pm
+++ RefElem.pm
@@ -1,15 +1,18 @@
 package Array::RefElem;
 
 use strict;
-use vars qw(@ISA @EXPORT $VERSION);
+use vars qw(@ISA @EXPORT_OK $VERSION);
 
 require Exporter;
 require DynaLoader;
 @ISA = qw(Exporter DynaLoader);
-@EXPORT_OK = qw();
+@EXPORT_OK = qw(av_store av_push hv_store);
 
 $VERSION = '0.01';
 
 Array::RefElem->bootstrap($VERSION);
 
 1;
+
+__END__
+
